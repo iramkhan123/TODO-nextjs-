e5a2dd7f5b415302a8dd83c3b496e82a838fa878
iramkhan123 <iramxyzkhan@gmail.com>
2023-11-24 23:13:28 +0530

    third

--- components/modal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Modal,Alert,Navbar,Button,TextInput,Card,Label,Textarea,Dropdown} from "flowbite-react";
-function modal() {
-  return (
-   
-  )
-}
-
-export default modal
